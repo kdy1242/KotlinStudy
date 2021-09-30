a5c8be74322d8e6449e191bd3d96dd91bf2a86f6
kdy1242 <dy12311229@gmail.com>
2021-09-30 22:36:58 +0900

    Navigation View

--- app/src/main/java/com/example/kotlinstudy/MainActivity.kt
+++ app/src/main/java/com/example/kotlinstudy/MainActivity.kt
@@ -3,33 +3,46 @@ package com.example.kotlinstudy
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.view.MenuItem
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.ListView
 import android.widget.Toast
+import androidx.core.view.GravityCompat
 import com.example.kotlinstudy.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
-    var UserList = arrayListOf<User>(
-        User(R.drawable.android, "자바", "18", "안녕하세요"),
-        User(R.drawable.android2, "자바스크립트", "21", "반갑다"),
-        User(R.drawable.android, "코틀린", "24", "집가고싶다"),
-        User(R.drawable.android2, "스위프트", "27", "안녕"),
-        User(R.drawable.android, "안드로이드", "25", "심심하다"),
-        User(R.drawable.android2, "리액트 네이티브", "12", "자고싶다")
-    )
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {    // 액티비티의 실행 시작 지점
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val Adapter = UserAdapter(this, UserList)
-        listView.adapter = Adapter
+        btn_navi.setOnClickListener {
+            layout_drawer.openDrawer(GravityCompat.START)   // START : left, END : right 랑 같은 말
+        }
 
-        listView.onItemClickListener = AdapterView.OnItemClickListener { parent, view, position, id ->
-            val selectItem = parent.getItemAtPosition(position) as User
-            Toast.makeText(this, selectItem.name, Toast.LENGTH_SHORT).show()
+        naviView.setNavigationItemSelectedListener(this)    // 네비게이션 메뉴 아이템에 클릭 속성 부여
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {  // 네비게이션 메뉴 아이템 클릭 시 수행
+        when (item.itemId){
+            R.id.access -> Toast.makeText(applicationContext, "접근성", Toast.LENGTH_SHORT).show()
+            R.id.email -> Toast.makeText(applicationContext, "이메일", Toast.LENGTH_SHORT).show()
+            R.id.message -> Toast.makeText(applicationContext, "메시지", Toast.LENGTH_SHORT).show()
+        }
+        layout_drawer.closeDrawers()    // 네비게이션 뷰 닫기
+        return false
+    }
+
+    override fun onBackPressed() {  // 백버튼 누를 시 수행하는 메소드
+        if (layout_drawer.isDrawerOpen(GravityCompat.START)){
+            layout_drawer.closeDrawers()
+        }
+        else {
+            super.onBackPressed()   // 일반 백버튼 기능 실행 (finish)
         }
     }
 }
