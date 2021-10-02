7f4e34239e1ef7b34f474831547eb1624a28489c
kdy1242 <dy12311229@gmail.com>
2021-10-02 22:41:18 +0900

    카메라 끝

--- app/src/main/java/com/example/kotlinstudy/MainActivity.kt
+++ app/src/main/java/com/example/kotlinstudy/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.kotlinstudy
 
+import android.app.Activity
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
 import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
@@ -9,12 +13,14 @@ import android.provider.MediaStore
 import android.webkit.PermissionRequest
 import android.webkit.WebChromeClient
 import android.webkit.WebViewClient
+import android.widget.ActionMenuView
 import android.widget.Toast
 import androidx.core.content.FileProvider
 import com.gun0912.tedpermission.PermissionListener
 import com.gun0912.tedpermission.TedPermission
 import kotlinx.android.synthetic.main.activity_main.*
 import java.io.File
+import java.io.FileOutputStream
 import java.io.IOException
 import java.net.URI
 import java.text.SimpleDateFormat
@@ -45,11 +51,13 @@ class MainActivity : AppCompatActivity(){
                     null
                 }
                 photoFile?.also {
-                    val photoURI: Uri? = FileProvider.getUriForFile(
+                    val photoURI: Uri = FileProvider.getUriForFile(
                         this,
                         "com.example.kotlinstudy.fileprovider",
                         it
                     )
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
                 }
             }
         }
@@ -80,5 +88,39 @@ class MainActivity : AppCompatActivity(){
             .setDeniedMessage("권한을 거부하셨습니다. [앱 설정] -> [권한] 항목에서 허용해주세요.")
             .setPermissions(android.Manifest.permission.WRITE_EXTERNAL_STORAGE, android.Manifest.permission.CAMERA)
             .check()
+    }
+
+    // startActivityForResult 를 통해서 기본 카메라 앱으로부터 받아온 사진 결과 값
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
+            val bitmap: Bitmap
+            val file = File(curPhotoPath)
+            if (Build.VERSION.SDK_INT < 28) {   // 안드로이드 9.0 (Pie) 버전보다 낮을 경우
+                bitmap = MediaStore.Images.Media.getBitmap(contentResolver, Uri.fromFile(file))
+            } else {    // 안드로이드 9.0 (Pie) 버전보다 높을 경우
+                val decode = ImageDecoder.createSource(
+                    this.contentResolver,
+                    Uri.fromFile(file)
+                )
+                bitmap = ImageDecoder.decodeBitmap(decode)
+                iv_profile.setImageBitmap(bitmap)
+            }
+            savePhoto(bitmap)
+        }
+    }
+
+    private fun savePhoto(bitmap: Bitmap) {
+        val folderPath = Environment.getExternalStorageDirectory().absolutePath + "/Pictures/"  // 사진폴더로 저장하기 위한 경로 선언
+        val timestamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val fileName = "${timestamp}.jpeg"
+        val folder = File(folderPath)
+        if (!folder.isDirectory) {  // 현재 해당 경로에 폴더가 존재하지 않는다면
+            folder.mkdirs() // make directory 줄임말로 해당 경로에 폴더 자동으로 새로 만들기
+        }
+        val out = FileOutputStream(folderPath + fileName)
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out)
+        Toast.makeText(this@MainActivity, "tkwls.", Toast.LENGTH_SHORT).show()
     }
 }
