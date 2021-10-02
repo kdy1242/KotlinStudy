fc26c670d9230be24792d3d73535f3e257b881ef
kdy1242 <dy12311229@gmail.com>
2021-10-02 21:49:56 +0900

    카메라 촬영, 이미지 파일 생성

--- app/src/main/java/com/example/kotlinstudy/MainActivity.kt
+++ app/src/main/java/com/example/kotlinstudy/MainActivity.kt
@@ -1,14 +1,24 @@
 package com.example.kotlinstudy
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import android.webkit.PermissionRequest
 import android.webkit.WebChromeClient
 import android.webkit.WebViewClient
 import android.widget.Toast
+import androidx.core.content.FileProvider
 import com.gun0912.tedpermission.PermissionListener
 import com.gun0912.tedpermission.TedPermission
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
+import java.io.IOException
+import java.net.URI
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity(){
     val REQUEST_IMAGE_CAPTURE = 1   // 카메라 사진 촬영 요청코드
@@ -19,7 +29,38 @@ class MainActivity : AppCompatActivity(){
         setContentView(R.layout.activity_main)
 
         setPermission() // 권한을 수행하는 메소드 수행
+        btn_camera.setOnClickListener {
+            takeCapture()   // 기본 카메라 앱을 실행하여 사진 촬영
+        }
+    }
 
+    // 카메라 촬영
+    private fun takeCapture() {
+        // 기본 카메라 앱 실행
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            takePictureIntent.resolveActivity(packageManager)?.also {
+                val photoFile: File? = try {
+                    createImageFile()
+                } catch (ex: IOException) {
+                    null
+                }
+                photoFile?.also {
+                    val photoURI: Uri? = FileProvider.getUriForFile(
+                        this,
+                        "com.example.kotlinstudy.fileprovider",
+                        it
+                    )
+                }
+            }
+        }
+    }
+
+    // 이미지 파일 생성
+    private fun createImageFile(): File? {
+        val timestamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val storageDir: File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile("JPEG_${timestamp}_", ".jpg", storageDir)
+            .apply {curPhotoPath = absolutePath}
     }
 
     // 테드 퍼미션 설정
