d07d5fd5e2608d2267f041445b3a5b0925301d12
kdy1242 <dy12311229@gmail.com>
2021-10-03 23:54:50 +0900

    RecyclerView - itemView 클릭

--- app/src/main/java/com/example/kotlinstudy/ProfileAdapter.kt
+++ app/src/main/java/com/example/kotlinstudy/ProfileAdapter.kt
@@ -5,12 +5,19 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 
 class ProfileAdapter (val profileList: ArrayList<Profiles>) : RecyclerView.Adapter<ProfileAdapter.CustomViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProfileAdapter.CustomViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false)
-        return CustomViewHolder(view)
+        return CustomViewHolder(view).apply {
+            itemView.setOnClickListener {
+                val curPos : Int = adapterPosition
+                val profile: Profiles = profileList.get(curPos)
+                Toast.makeText(parent.context, "이름 : ${profile.name}\n나이 : ${profile.age}\n직업 : ${profile.job}", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
     override fun onBindViewHolder(holder: ProfileAdapter.CustomViewHolder, position: Int) {
