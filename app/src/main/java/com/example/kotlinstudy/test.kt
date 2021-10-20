c48b2b669d2a56faffa4974a0a883f9098626796
kdy1242 <dy12311229@gmail.com>
2021-10-20 22:57:57 +0900

    옵저버 패턴

--- app/src/main/java/com/example/kotlinstudy/test.kt
+++ app/src/main/java/com/example/kotlinstudy/test.kt
@@ -1,40 +1,43 @@
 package com.example.kotlinstudy
 
-// 오브젝트
-// 생성자 없이 객체를 직접 만들어냄
+// 익명객체와 옵저버 패턴
 
-// 클래스는 인스턴스 객체를 만들기 위한 틀 -> 생성자로 인스턴스 만들어야 속성이나 함수 사용가능
-// 단하나의 객체만으로 공통적인 속성과 함수를 사용해야하는 코드에서는 오브젝트 사용
+// 이벤트가 일어나는것을 감시하는 감시자 역할(키의 입력, 터치의 발생, 데이터 수신 등)
+// 이벤트 - 함수로 직접 요청하지 않았지만 시스템 또는 루틴에 의해서 발생하는 동작
+// 옵저버 패턴 - 이벤트가 발생할때마다 즉각적으로 처리할수있도록 만드는 프로그래밍 패턴
 
-// 오브젝트로 선언된 객체는 최초사용시 자동으로 생성, 이후에는 코드 전체에서 공용으로 사용 ㄱㄴ -> 프로그램이 종료되기전까지 공통적으로 사용될 내용을묶어 사용
+// 옵저버 패턴 구현할땐 두개의 클래스 필요함
+// 1. 이벤트 수신하는 클래스
+// 2. 이벤트의 발생 및 전달하는 클래스
 
-// Companion Object -> 기존 클래스 안에서도 오브젝트 만들수 ㅇㅇ
-// 클래스의 인스턴스 기능은 그대로 사용, 인스턴스간에 공용으로 사용할 속성과 함수를 만드는것(기존의 static 멤버와 비슷)
+// 2에서 이벤트가 발생할때 1에있는 이벤트 처리하는 함수를 호출
+// 하지만 1은 2를 참조할수있지만 2는 1을 참조할방법이 없음 -> 인터페이스로 해결
+// 이때 인터페이스를 observer(코틀린에서는 listener), 이벤트를 넘겨주는 행위는 callback
 
 fun main() {
-    var a = FoodPoll("짜장")
-    var b = FoodPoll("짬뽕")
-
-    a.vote()
-    a.vote()
-
-    b.vote()
-    b.vote()
-    b.vote()
-
-    println("${a.name} : ${a.count}")
-    println("${b.name} : ${b.count}")
-    println("총계 : ${FoodPoll.total}")   // 서로 다른 인스턴스임에도 companion object 내에있는 total 을 공유하고 있기 때문에 누적 ㄱㄴ
+    EventPrinter().start()
 }
 
-class FoodPoll (val name: String) {
-    companion object {
-        var total = 0
-    }
-    var count = 0
+interface EventListener {
+    fun onEvent(count: Int)
+}
 
-    fun vote(){
-        total++
-        count++
+class Counter(var listener: EventListener) {
+    fun count() {
+        for(i in 1..100) {
+            if(i % 5 == 0) listener.onEvent(i)
+        }
+    }
+}
+
+class EventPrinter: EventListener {
+    override fun onEvent(count: Int) {
+        print("${count}-")
+    }
+
+    fun start() {
+        val counter = Counter(this)
+        // this - EventPrinter 객체 자신을 나타내지만 받는쪽에서 EventListener 만 요구했기 때문에 EventListener 구현부만 넘겨주게됨
+        counter.count()
     }
 }
