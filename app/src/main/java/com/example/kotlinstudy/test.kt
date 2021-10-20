107aa0f8c62d5c06efe1c2464e577fbb53a801ab
kdy1242 <dy12311229@gmail.com>
2021-10-20 22:45:29 +0900

    Companion Object

--- app/src/main/java/com/example/kotlinstudy/test.kt
+++ app/src/main/java/com/example/kotlinstudy/test.kt
@@ -8,27 +8,33 @@ package com.example.kotlinstudy
 
 // 오브젝트로 선언된 객체는 최초사용시 자동으로 생성, 이후에는 코드 전체에서 공용으로 사용 ㄱㄴ -> 프로그램이 종료되기전까지 공통적으로 사용될 내용을묶어 사용
 
+// Companion Object -> 기존 클래스 안에서도 오브젝트 만들수 ㅇㅇ
+// 클래스의 인스턴스 기능은 그대로 사용, 인스턴스간에 공용으로 사용할 속성과 함수를 만드는것(기존의 static 멤버와 비슷)
+
 fun main() {
-    println(Counter.count)
+    var a = FoodPoll("짜장")
+    var b = FoodPoll("짬뽕")
 
-    Counter.countUp()
-    Counter.countUp()
+    a.vote()
+    a.vote()
 
-    println(Counter.count)
+    b.vote()
+    b.vote()
+    b.vote()
 
-    Counter.clear()
-
-    println(Counter.count)
+    println("${a.name} : ${a.count}")
+    println("${b.name} : ${b.count}")
+    println("총계 : ${FoodPoll.total}")   // 서로 다른 인스턴스임에도 companion object 내에있는 total 을 공유하고 있기 때문에 누적 ㄱㄴ
 }
 
-object Counter {
+class FoodPoll (val name: String) {
+    companion object {
+        var total = 0
+    }
     var count = 0
 
-    fun countUp() {
+    fun vote(){
+        total++
         count++
-    }
-
-    fun clear() {
-        count = 0
     }
 }
