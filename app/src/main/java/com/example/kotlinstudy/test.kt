ae8396b3546a037879a1f130fb9af113e5868bbc
kdy1242 <dy12311229@gmail.com>
2021-10-21 23:09:21 +0900

    클래스의 다형성

--- app/src/main/java/com/example/kotlinstudy/test.kt
+++ app/src/main/java/com/example/kotlinstudy/test.kt
@@ -1,46 +1,49 @@
 package com.example.kotlinstudy
 
-// 익명객체와 옵저버 패턴
+// 클래스의 다형성
 
-// 이벤트가 일어나는것을 감시하는 감시자 역할(키의 입력, 터치의 발생, 데이터 수신 등)
-// 이벤트 - 함수로 직접 요청하지 않았지만 시스템 또는 루틴에 의해서 발생하는 동작
-// 옵저버 패턴 - 이벤트가 발생할때마다 즉각적으로 처리할수있도록 만드는 프로그래밍 패턴
+// 다형성 - 클래스의 상속관계에서 오는 인스턴스의 호환성을 적극 활용할 수 잇는 기능
+// 수퍼클래스가 같은 인스턴스를 한번에 관리하거나 인터페이스를 구현하여 사용하는 코드에서도 사용됨- 이해꼭필요
 
-// 옵저버 패턴 구현할땐 두개의 클래스 필요함
-// 1. 이벤트 수신하는 클래스
-// 2. 이벤트의 발생 및 전달하는 클래스
+// 업캐스팅 - 상위 자료형으로 변환
+// 다운캐스팅 - 하위자료형으로 변환
 
-// 2에서 이벤트가 발생할때 1에있는 이벤트 처리하는 함수를 호출
-// 하지만 1은 2를 참조할수있지만 2는 1을 참조할방법이 없음 -> 인터페이스로 해결
-// 이때 인터페이스를 observer(코틀린에서는 listener), 이벤트를 넘겨주는 행위는 callback
+// 업캐스팅은 상위 자료형에 담는것으로 동작, 다운캐스팅은 별도의 연산자가 필요함 -> as, is
+// as - 변수를 호환되는 자료형으로 변환해주는 캐스팅 연산자, 코드 내에서 사용할시 즉시 자료형을 변환해줌 / 변환된 자료형을 반환도 해줌
+// is - 변수가 자료형에 호환되는지를 먼저 체크한후 변환해주는 캐스팅 연산자, 조건문 내에서 사용됨
 
-// EventPrinter 가 EventListener 를 상속받아 구현하지 않고 임시로 만든 별도의 EventListener 객체를 대신 넘겨줄 수도 잇음
-// -> 익명객체 (이름이 없는 객체)
 fun main() {
-    EventPrinter().start()
+    var a = Drink()
+    a.drink()
+
+    var b: Drink = Cola()
+    b.drink()
+
+    if(b is Cola) { // is는 조건문 안에서만 잠시 다운캐스팅됨
+        b.washDishes()
+    }
+
+    var c = b as Cola
+    c.washDishes()
+    b.washDishes()  // as를 사용하면 반환값뿐만아니라 변수 자체도 다운캐스팅되기 때문에 에러 ㄴㄴ
 }
 
-interface EventListener {
-    fun onEvent(count: Int)
-}
+open class Drink {
+    var name = "음료"
 
-class Counter(var listener: EventListener) {
-    fun count() {
-        for(i in 1..100) {
-            if(i % 5 == 0) listener.onEvent(i)
-        }
+    open fun drink(){
+        println("${name}를 마십니다")
     }
 }
 
-class EventPrinter {
-    fun start() {
-        val counter = Counter(object: EventListener{
-            override fun onEvent(count: Int) {
-                print("${count}-")
-            }
-        })
-        counter.count()
+class Cola: Drink() {
+    var type = "콜라"
+
+    override fun drink() {
+        println("${name}중에 ${type}를 마십니다")
+    }
+
+    fun washDishes() {
+        println("${type}로 설거지를 합니다")
     }
 }
-
-// 옵저버패턴은 이벤트를 기반으로 동작하는 모든 코드에서 광범위하게 쓰이는 방식이므로 구조를 이해하는것이 중요
