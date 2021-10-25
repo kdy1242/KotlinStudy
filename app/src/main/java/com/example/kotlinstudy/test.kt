bc3bec4836d3bb045d5d9e4abae74e2296c443bf
kdy1242 <dy12311229@gmail.com>
2021-10-25 23:15:42 +0900

    null값 처리하는 방법

--- app/src/main/java/com/example/kotlinstudy/test.kt
+++ app/src/main/java/com/example/kotlinstudy/test.kt
@@ -1,52 +1,34 @@
 package com.example.kotlinstudy
 
-// 문자열을 다루는 법
+// null 값 처리하는 방법
+// nullable 변수를 null 상태로 속성이나 함수를 쓰려고하면 null pointer exception 이 발생함
+// nullable 변수를 사용할 때는 null 체크 없이는 코드가 컴파일되지 않음
+
+// ?. (null safe operator)
+// 참조연산자를 실행하기 전에 먼저 객체가 null 인지 확인부터 하고
+// 객체가 null 이라면 뒤따라오는 구문을 실행하지 않는 연산자
+
+// ?: (elvis operator)
+// 객체가 null 이 아니라면 그대로 사용하지만
+// null 이라면 연산자 우측의 객체로 대체되는 연산자
+
+// !!. (non-null assertion operator)
+// 참조연산자를 사용할때 null 여부를 컴파일시 확인하지 않도록하여
+// 런타임시 null pointer exception 이 나도록 의도적으로 방치하는 연산자
 
 fun main() {
-    val test1 = "Test.Kotlin.String"
+    var a: String? = "Kotlin Exam"
 
-    println(test1.length)   // 문자열의 길이
+//    println(a?.toUpperCase())
+//    println(a?:"default".toUpperCase())
+//    println(a!!.toUpperCase())
 
-    println(test1.toLowerCase())    // 영문 소문자로 문자열 전체를 변환하여 반환
-    println(test1.toUpperCase())    // 영문 대문자로 문자열 전체를 변환하여 반환
 
-    val test2 = test1.split(".")
-    println(test2)   // 특정 문자열을 기준으로 문자열을 여러개로 나눠 배열에 담기
+    // null safe 연산자는 스코프 함수와 사용하면 더 편리함
 
-    // 문자열이 들어가있는 배열을 다시 하나의 문자열로 합침
-    println(test2.joinToString())
-    println(test2.joinToString("-"))
-
-    // 문자열 일부분만 사용
-    println(test1.substring(5..10))
-
-    // 문자열이 비어있는지 여부를 판단하여 boolean 값으로 반환
-    val nullString: String? = null
-    val emptyString = ""
-    val blankString = " "
-    val normalString = "A"
-
-    println(nullString.isNullOrEmpty())     // null 이거나 empty 이면 true 반환
-    println(emptyString.isNullOrEmpty())
-    println(blankString.isNullOrEmpty())
-    println(normalString.isNullOrEmpty())
-
-    println()
-
-    println(nullString.isNullOrBlank())     // null 이거나 blank 이면 true 반환
-    println(emptyString.isNullOrBlank())    // 공백 문자 포함(space, tab, line feed 등등 눈에 직접적으로 보이지 않는 문자)
-    println(blankString.isNullOrBlank())
-    println(normalString.isNullOrBlank())
-
-    var test3 = "kotlin.kt"
-    var test4 = "java.java"
-
-    println(test3.startsWith("java"))   // 지정한 문자열로 시작하면 true 반환
-    println(test4.startsWith("java"))
-
-    println(test3.endsWith(".kt"))      // 지정한 문자열로 끝나면 true 반환
-    println(test4.endsWith(".kt"))
-
-    println(test3.contains("lin"))      // 지정한 문자열이 포함되면 true 반환
-    println(test4.contains("lin"))
+    a?.run{
+        println(toUpperCase())
+        println(toLowerCase())
+    }   // a 가 null 이면 스코프 함수 전체가 수행되지 않음
+    // null 을 체크하기 위해 if 문 대신 사용하면 상당히 편리한 기능
 }
