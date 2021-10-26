2b790d7e2bc7fbeec7ab1f84d90c747bd2d9078b
kdy1242 <dy12311229@gmail.com>
2021-10-26 23:14:13 +0900

    default arguments, named arguments

--- app/src/main/java/com/example/kotlinstudy/test.kt
+++ app/src/main/java/com/example/kotlinstudy/test.kt
@@ -2,19 +2,19 @@ package com.example.kotlinstudy
 
 // 함수의 argument 다루는 방법
 
-// overloading - 같은 스코프 안에서 같은 이름의 함수를 여러개 만들기 ㄱㄴ
-// 이름이 같더라도 패러미터의 자료형이 다르거나 갯수가 다르다면 서로다른함수로 동작 가능
-// 다만 패러미터의 이름만 다르게묶고 자료형과 갯수가 동일하면 오버로딩 못함
+// 패러미터를 받아야하는 함수지만 별다른 패러미터가 없더라도 기본값으로 동작해야한다면
+// default arguments 사용
+
+// named arguments - 패러미터의 순서와 관계없이 패러미터의 이름을 사용하여 직접 패러미터의 값을 할당하는 기능
 
 fun main() {
-    read(7)
-    read("감사합니다")
+    deliveryItem("짬뽕")
+    deliveryItem("책", 3)
+    deliveryItem("노트북", 30, "학교")
+
+    deliveryItem("선물", destination = "친구집") // named arguments
 }
 
-fun read(x: Int) {
-    println("숫자 &x 입니다")
-}
-
-fun read(x: String) {
-    println(x)
+fun deliveryItem(name: String, count: Int = 1, destination: String = "집") {
+    println("${name}, ${count}개를 ${destination}에 배달하였습니다")
 }
