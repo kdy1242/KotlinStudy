f873dfbe030ef7b44092229d4df0f7d51fef5e75
kdy1242 <dy12311229@gmail.com>
2021-10-27 23:40:41 +0900

    중첩클래스와 내부클래스

--- app/src/main/java/com/example/kotlinstudy/test.kt
+++ app/src/main/java/com/example/kotlinstudy/test.kt
@@ -1,16 +1,50 @@
 package com.example.kotlinstudy
 
-// infix 함수
-// 연산자처럼 쓸수있는 infix 함수
+// 중첩클래스와 내부클래스
+
+// 중첩클래스 - 하나의 클래스가 다른 클래스의 기능과 강하게 연관되어잇다는 의미를 전달하기위해 만들어진 형식
+// 사용할땐 외부클래스이름.중첩클래스이름
+
+// 내부클래스 - 중첩클래스에 inner 라는 키워드를 붙인 내부클래스는
+// 혼자서 객체를 만들수는 없고 외부클래스에 객체가 있어야만 생성과 사용이 가능한 클래스
+
+// 중첩클래스는 형태만 내부에 존재할뿐 실질적으로는 서로 내용을 공유할수없는 별개의 클래스이다
+// 내부클래스는 외부클래스 객체 안에서 사용되는 클래스이므로 외부클래스객체의 속성이나 함수를 사용할수 있다
 
 fun main() {
-    println(6 multiply 4)   // 좌측에 붙은 6이 infix 함수가 적용되는 객체 자신 (this)
-                            // 우측에 붙은 4가 패러미터인 x에 해당함
+    Outer.Nested().introduce()
 
-    println(6.multiply(4))
+    val outer = Outer()
+    val inner = outer.Inner()
 
-    // 클래스 안에서 infix 함수를 선언할때는 적용할 클래스가 자기 자신이므로 클래스이름은 쓰지않음
+    inner.introduceInner()
+    inner.introduceOuter()
+
+    outer.text = "Changed Outer Class"
+    inner.introduceOuter()
 }
-infix fun Int.multiply(x: Int): Int = this * x
-// 함수를 정의할때 앞에 infix 붙이기
-// 함수 이름을 infix 함수가 적용될 자료형.이름으로 지정
+
+class Outer {
+    var text = "Outer Class"
+
+    class Nested {
+        fun introduce() {
+            println("Nested Class")
+        }
+    }
+
+    inner class Inner {
+        var text = "Inner Class"
+
+        fun introduceInner() {
+            println(text)
+        }
+
+        fun introduceOuter() {
+            println(this@Outer.text)
+        }
+    }
+}
+
+// 중첩클래스와 내부클래스는 클래스간에 연계성을 표현하여 코드의 가독성및 작성 편의성이 올라갈수 잇으므로
+// 적절한 상황에서 사용하는것이 좋음
