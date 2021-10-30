d5921da4ce74e901d44c2a8295c5e6f47b5e5fb8
kdy1242 <dy12311229@gmail.com>
2021-10-30 23:36:58 +0900

    Set

--- app/src/main/java/com/example/kotlinstudy/test.kt
+++ app/src/main/java/com/example/kotlinstudy/test.kt
@@ -1,34 +1,27 @@
 package com.example.kotlinstudy
 
-// Enum Class
+// Set
+// 코틀린이 기본적으로 지원하는 컬렉션 클래스
 
-// enumerated type(열거형)
-// enum class 내의 상태를 구분하하기위한 객체들을 이름을 붙여 여러개 생성해두고
-// 그중 하나의 상태를 선택하여 나타내기위한 클래스
+// List 와 달리 순서가 정렬되지 않으며 중복이 허용되지 않는 컬렉션
+// 인덱스로 위치 지정하여 객체를 참조할수 없음, contains 로 객체가 set 안에 존재하는지를 확인하는 식으로만 사용
 
-// enum 클래스 안의 객체들은 관행적으로 상수를 나타낼때 사용하는 대문자로 기술
-
-// enum 의 객체들을 고유한 속성을 가질수잇다
-// enum 의 생성자를 만들어 속성을 받도록하면 객체를 선언할때 속성도 설정할수잇음
-
-// 일반클래스처럼 함수도 가질수잇다
-// 객체의 선언이 끝나는 위치에 세미콜론을 추가하고 함수를 기술
+// Set<out T>      MutableSet<T>
+// 객체의 추가, 삭제가 가능한지 여부에 따라 사용
+// 추가, 삭제 - add, remove
 
 fun main() {
-    var state = State.SING
-    println(state)
+    val a = mutableSetOf("귤", "바나나", "키위")
 
-    state = State.SLEEP
-    println(state.isSleeping())
+    for(item in a) {
+        println("${item}")
+    }
 
-    state = State.EAT
-    println(state.message)
-}
+    a.add("자몽")
+    println(a)
 
-enum class State(val message: String) {
-    SING("노래를 부릅니다"),
-    EAT("밥을 먹습니다"),
-    SLEEP("잠을 잡니다");
+    a.remove("바나나")
+    println(a)
 
-    fun isSleeping() = this == State.SLEEP
+    println(a.contains("귤"))
 }
