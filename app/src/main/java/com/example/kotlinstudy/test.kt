e7584f7451c112928ae01dce56a5eec4e0ac2059
kdy1242 <dy12311229@gmail.com>
2021-11-01 23:34:41 +0900

    컬렉션함수 3

--- app/src/main/java/com/example/kotlinstudy/test.kt
+++ app/src/main/java/com/example/kotlinstudy/test.kt
@@ -1,19 +1,16 @@
 package com.example.kotlinstudy
 
-// 컬렉션함수 2
+// 컬렉션함수 3
 
 fun main() {
-    data class Person(val name: String, val birthYear: Int)
+    val numbers = listOf(-3, 7, 2, -10, 1)
 
-    val personList = listOf(Person("유길동", 1990),
-                            Person("조길동", 1992),
-                            Person("주길동", 1999),
-                            Person("최길동", 2003))
+    println(numbers.flatMap { listOf(it * 10, it + 10) })   // 아이템마다 만들어진 컬렉션을 합쳐서 반환
 
-    println(personList.associateBy { it.birthYear })    // 아이템에서 key 를 추출하여 map 으로 변환하는 함수
-    println(personList.groupBy { it.name })     // key 가 같은 아이템끼리 배열로 묶어 map 으로 만드는 함수
+    println(numbers.getOrElse(1) { 50 })    // 인덱스 위치에 아이템이 있으면 아이템을 반환, 아닌 경우 지정한 기본값을 반환
+    println(numbers.getOrElse(10) { 50 })   // 컬렉션 두개의 아이템을 1:1로 매칭하여 새 컬렉션을 만들어줌
 
-    val (over98, under98) = personList.partition { it.birthYear > 1998 }    // 아이템에 조건을 걸어 두개의 컬렉션으로 나누어 줌
-    println(over98)
-    println(under98)
+    val names = listOf("A", "B", "C", "D")
+
+    println(names zip numbers)
 }
