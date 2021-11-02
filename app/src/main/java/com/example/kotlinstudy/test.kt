a75d01483013541bd63005924764fe85b666e082
kdy1242 <dy12311229@gmail.com>
2021-11-02 15:56:02 +0900

    지연 대리자 속성

--- app/src/main/java/com/example/kotlinstudy/test.kt
+++ app/src/main/java/com/example/kotlinstudy/test.kt
@@ -1,35 +1,24 @@
 package com.example.kotlinstudy
 
 // 변수의 고급 기술
-// 늦은 초기화
+// 지연 대리자 속성 (lazy delegate properties)
+// 변수를 사용하는 시점까지 초기화를 자동으로 늦춰줌
 
-// lateinit - 일단 변수만 선언하고 초기값의 할당은 나중에 할수 잇도록 하는 키워드 (var 앞에 써주기)
+// val 변수에 by 라는 키워드를 사용하여 lazy 라는 람다함수형태의 초기화함수를 사용하는 형태
+// 코드에서는 선언시 즉시 객체를 생성 및 할당하여 변수를 초기화하는 형태를 가지고잇지만
+// 실제 실행시에는 val 변수를 사용하는 시점에 초기화 과정을 진행함으로서 코드의 실행시간을 최적화할수있다
 
-// lateinit var 변수의 제한사항
-// 초기값 할당 전까지 변수를 사용할 수 없음 (에러 발생)
-// 기본 자료형에는 사용할 수 없음 (String 클래스에는 사용 가능)
-
-// lateinit 변수의 초기화를 하였는지 여부를 확인할때는 ::변수이름.isInitialized
+// 람다함수로 초기화가 진행되므로 함수안에 여러개의 구문이 들어갈수있으며 맨 마지막구문의 결과가 변수에 할당됨
 
 fun main() {
 
-    val a = LateInitSample()
-
-    println(a.getLateInitText())
-    a.text = "새로 할당한 값"
-    println(a.getLateInitText())
-
-}
-
-class LateInitSample {
-    lateinit var text: String
-
-    fun getLateInitText(): String {
-        if(::text.isInitialized) {
-            return text
-        }
-        else {
-            return "기본값"
-        }
+    val number: Int by lazy {
+        println("초기화를 합니다")
+        7
     }
+
+    println("코드를 시작합니다")
+    println(number)
+    println(number)     // 이미 초기화를 했기때문에 다시 초기화구문 실행하지 않음
+
 }
